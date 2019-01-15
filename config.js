53be74a1db8d424c6b0ae49560aa5a3c320e20e6
Tomasz Olejniczak <snustor@gmail.com>
2019-01-15 22:21:18 +0100

    Project files addition

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '629426551113-uk4apqv49e82s4dobapv57odm095igop.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'BNeCvNWMD1DaL9ftF3_tnYNj',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
